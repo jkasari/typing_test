dcbe5d0ba3a7a6ce4f25ebf57e6231a75d4bbb46
jkasari <jkasari@outlook.com>
2022-03-28 20:42:53 -0700

    Made it better!!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import wx
 import random
 import time
@@ -30,17 +31,27 @@ class FuncPanel(wx.Panel):
         self.data_list = []
         self.start = time.time()
         self.time_limit = 2.0
+        self.temp = ''
+        self.result = 0
 
         #Creates a prompt character for the user to type in
-        self.prompt_text = wx.StaticText(self, label=(''), pos=(300, 100), size=(100, 100))
+        self.prompt_text = wx.StaticText(self, label=(''), pos=(270, 100), size=(100, 100), style=wx.ALIGN_CENTER)
         self.prompt_font = wx.Font(pointSize= 60, family=wx.FONTFAMILY_DECORATIVE, style=wx.FONTSTYLE_ITALIC, weight=wx.FONTWEIGHT_NORMAL, underline=False, faceName="", encoding=wx.FONTENCODING_DEFAULT)
         self.prompt_text.SetFont(self.prompt_font)
         self.refresh_list()
 
+        # create a restart button that appears only when the game is over.
+        # Also gives you a final score of how my times you where out of time.
+        self.restart_button = wx.Button(self, label='Restart', pos=(285, 370), size=(60, 30), style=wx.ALIGN_CENTRE)
+        self.result_text = wx.StaticText(self, label='', pos=(265, 420), size=(100, 50), style=wx.ALIGN_CENTER)
+        self.result_text.Hide()
+        self.restart_button.Hide()
+        self.Bind(wx.EVT_BUTTON, self.restart_game)
+
         #Creates an entry box for the user to type their answer into
-        self.entry = wx.TextCtrl(parent=self, id=-1, value="", pos=(310, 250), size=(30, 30))
+        self.entry = wx.TextCtrl(parent=self, id=-1, value="", pos=(300, 250), size=(30, 30))
         self.Bind(wx.EVT_TEXT, self.change_prompt)
-        time.sleep(self.time_limit)
+
 
 
     # check to see if the text in the entry matchs that in the prompt
@@ -53,26 +64,45 @@ class FuncPanel(wx.Panel):
     def check_time(self):
         time_diff = time.time() - self.start
         print(time_diff)
-        if time_diff < self.time_limit:
+        if time_diff < self.time_limit * len(self.temp):
             temp_char = self.prompt_text.GetLabel()
             print(f"{temp_char} gone!")
             self.data_list.remove(temp_char)
+            self.SetBackgroundColour("Green")
+        else:
+            self.SetBackgroundColour("Red")
+            self.result += 1
+        self.Refresh()
 
     # If there is data in the list update the prompt with the next random value from the list
     def update_prompt(self):
         if self.data_list:
-            temp = random.choice(self.data_list)
-            self.prompt_text.SetLabel(temp)
+            self.temp = random.choice(self.data_list)
+            self.prompt_text.SetLabel(self.temp)
             self.start = time.time()
         else:
-            self.refresh_list()
+            self.finish()
         self.entry.SetValue('')
 
     #Reset the list back the original contents and put the "Go" prompt back up
     def refresh_list(self):
-        self.data_list = ['`', '!', '@', '#', '$', '%', '^', '*', '(', ')', '-', '_', '+', '=']
+        self.data_list = ['`', '!', '@', '#', '$', '%', '^', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', ':', '|', '/', '<', '>', '->', ';', '.', ',']
         #self.data_list = ['a', 'b', 'c', 'd']
         self.prompt_text.SetLabel('Go')
+        self.result = 0
+
+    def finish(self):
+        self.result_text.SetLabel(self.get_result_string())
+        self.restart_button.Show()
+        self.result_text.Show()
+
+    def get_result_string(self):
+        return f"Your result is: {self.result - 1}"
+    
+    def restart_game(self, e):
+        self.restart_button.Hide()
+        self.result_text.Hide()
+        self.refresh_list()
 
 
 if __name__ == "__main__":
